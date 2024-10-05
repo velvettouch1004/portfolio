e620f4de04692cb2f1087521d8f1ffd6f43d191b
Lucas <velvettouch1004@gmail.com>
2024-10-05 14:17:06 -0500

    last

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -2,24 +2,21 @@ import React from "react";
 
 import classes from "./certifications.module.css";
 import CreateCertificate from "./CreateCertificate";
-import CertificatesData from "../../Data/CertificatesData"
+import CertificatesData from "../../Data/CertificatesData";
 import { useSelector } from "react-redux";
-const Certifications = (props) => {
+const Certifications = () => {
+  const nonThemeColor = useSelector((state) => state.nonThemeColor);
+  let certificationsList = CertificatesData.certificationsList;
 
-    const nonThemeColor=useSelector(state=>state.nonThemeColor);
-    let certificationsList = CertificatesData.certificationsList;
-
-    return (
-        <React.Fragment>
-            <h1 style={{color:nonThemeColor}}>Certifications</h1>
-            <div className={classes.certificateCard}>
-                {certificationsList.map((item, index) => {
-                    return (
-                        <CreateCertificate key={index} item={item}/>
-                    )
-                })}
-            </div>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <h1 style={{ color: nonThemeColor }}>Certifications</h1>
+      <div className={classes.certificateCard}>
+        {certificationsList.map((item, index) => {
+          return <CreateCertificate key={index} item={item} />;
+        })}
+      </div>
+    </React.Fragment>
+  );
+};
 export default Certifications;
